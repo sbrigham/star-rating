fd8a1e6279db50465e265f5aab8110854303824e
Spencer Brigham <sdbrigha@buffalo.edu>
2014-10-05 12:49:15 -0400

    Take responsibility out of directive, use a callback

--- public/app.js
+++ public/app.js
@@ -1,126 +1,96 @@
 'use strict';
 
 /**
- * @ngdoc overview
- * @name starRatingApp
- * @description
- * # starRatingApp
- *
- * Main module of the application.
- */
-angular
-    .module('starRatingApp', ['templates']);
-
-/**
  * @ngdoc function
  * @name starRatingApp.controller:MainCtrl
  * @description
  * # MainCtrl
  * Controller of the starRatingApp
  */
-angular.module('starRatingApp', ['sbRatingDirective'])
-    .controller('MainCtrl', ['$scope', 'Rating', function ($scope, Rating) {
-        if(! $scope.max) {
-            $scope.max = 5; // Default to 5 stars
-        }
-
-        // Light up the stars when ready
-        $scope.$watch(function() {
-            return $scope.$parent.initrating;
-        }, function(value) {
-            if (value != undefined) {
-                $scope.rating = value;
-                $scope.setStars(value);
-            }
-        });
-
-        $scope.stars = [];
-        var star = { class: 'star' };
-        for(var x=1; x<$scope.max+1; x++) {
-            var star_clone = angular.copy(star);
-            star_clone.star_num = x;
-            $scope.stars.push(star_clone);
-        }
-
-        $scope.hoverStar = function(hovered_star) {
-            angular.forEach($scope.stars, function(star, key) {
-                star.class = 'star'
-            });
-
-            angular.forEach($scope.stars, function(star, key) {
-                if (star.star_num <= hovered_star.star_num) {
-                    star.class = 'rating-star-active'
-                }
-            });
-        };
-        $scope.leaveHoverStar = function(current_star) {
-            if ($scope.rating == null) {
-                angular.forEach($scope.stars, function(star, key) {
-                    star.class = 'star';
-                });
-            } else {
-                angular.forEach($scope.stars, function(star, key) {
-                    if(star.star_num > $scope.rating) {
-                        star.class = 'star';
-                    } else if (current_star.star_num < $scope.rating) {
-                        star.class = 'rating-star-active';
-                    }
-                });
-            }
-        };
-
-        $scope.clickStar = function(clicked_star) {
-            if ($scope.rating == clicked_star.star_num) {
-                $scope.rating = null;
-
-                for (var star in $scope.stars) {
-                    star.class = '' // clear all extra classes
-                }
-            } else {
-                $scope.rating = clicked_star.star_num;
-                $scope.saveRating($scope.rating);
-
-                angular.forEach($scope.stars, function(star, key) {
-                    if (star.star_num <= $scope.rating) {
-                        star.class = 'rating-star-active'
-                    }
-                });
-            }
-        };
-
-        $scope.saveRating = function(rating) {
-            Rating.saveRating($scope.$parent.$parent.episode_id, rating);
-        }
-
-        $scope.setStars = function(star_value) {
-            angular.forEach($scope.stars, function(star) {
-                if (star.star_num <= star_value) {
-                    star.class = 'rating-star-active';
-                }
-            });
-        }
-    }]);
-
-angular.module('sbRatingDirective', [])
+angular.module('starRating', [])
     .directive('starRating', function() {
         return {
-            restrict : 'E',
-            templateUrl: 'partials/star-rating.html',
+            restrict: 'E',
             scope: {
-                max: '=',
-                initrating: '='
-            }
-        }
-    }).factory('Rating', ['$http', function($http) {
-        return {
-            saveRating: function(episode_id, rating) {
-                $http.post('/api/episode/'+episode_id+'/rating', { rating: rating })
-                    .success(function(){
-                        console.log('success');
-                    }).error(function() {
-                        console.log('fail');
-                        // log error
+              ratingChanged: '&'
+            },
+            templateUrl: 'partials/star-rating.html',
+            link: function($scope, $element, $attrs) {
+                $attrs.max = $attrs.max || 5;
+                $attrs.max = parseInt($attrs.max);
+
+                // Create the stars
+                $scope.stars = [];
+                var star = { class: 'star' };
+                for(var x=1; x<$attrs.max+1; x++) {
+                    var star_clone = angular.copy(star);
+                    star_clone.star_num = x;
+                    $scope.stars.push(star_clone);
+                }
+
+                $scope.setStars = function(star_value) {
+                    angular.forEach($scope.stars, function(star) {
+                        if (star.star_num <= star_value) {
+                            star.class = 'rating-star-active';
+                        }
                     });
+                }
+
+                // Set the initial rating
+                if ($attrs.initRating) {
+                    $attrs.initRating = parseInt($attrs.initRating);
+                    $scope.rating = $attrs.initRating;
+                    $scope.setStars($attrs.initRating);
+                }
+
+                $scope.hoverStar = function(hovered_star) {
+                    angular.forEach($scope.stars, function(star, key) {
+                        star.class = 'star'
+                    });
+
+                    angular.forEach($scope.stars, function(star, key) {
+                        if (star.star_num <= hovered_star.star_num) {
+                            star.class = 'rating-star-active'
+                        }
+                    });
+                };
+                $scope.leaveHoverStar = function(current_star) {
+                    if ($scope.rating == null) {
+                        angular.forEach($scope.stars, function(star, key) {
+                            star.class = 'star';
+                        });
+                    } else {
+                        angular.forEach($scope.stars, function(star, key) {
+                            if(star.star_num > $scope.rating) {
+                                star.class = 'star';
+                            } else if (current_star.star_num < $scope.rating) {
+                                star.class = 'rating-star-active';
+                            }
+                        });
+                    }
+                };
+
+                $scope.clickStar = function(clicked_star) {
+                    if ($scope.rating == clicked_star.star_num) {
+                        $scope.rating = null;
+
+                        for (var star in $scope.stars) {
+                            star.class = '' // clear all extra classes
+                        }
+                    } else {
+                        $scope.rating = clicked_star.star_num;
+
+                        angular.forEach($scope.stars, function(star, key) {
+                            if (star.star_num <= $scope.rating) {
+                                star.class = 'rating-star-active'
+                            }
+                        });
+                    }
+
+                    if ($scope.ratingChanged) {
+                        $scope.ratingChanged({rating: $scope.rating});
+                    }
+                };
             }
         }
-    }]);
+    });
